d2c5220faadba087b407c53a7735783168dfc168
sebalr <larrieu.sebastian@gmail.com>
2020-04-26 16:55:48 -0300

    Test removed

--- projects/ngx-feature-flags/src/lib/show-if-not-feature.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShowIfNotFeatureDirective } from './show-if-not-feature.directive';
-
-describe('ShowIfNotFeatureDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ShowIfNotFeatureDirective();
-    expect(directive).toBeTruthy();
-  });
-});
